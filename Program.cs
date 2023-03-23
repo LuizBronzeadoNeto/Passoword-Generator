fd2436e79e36bc5bb961f8f1437a9ccda9f293ce
TechnicalBark59 <69558292+TechnicalBark59@users.noreply.github.com>
2023-03-23 07:52:59 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("How many characters should the password have?");
-        int DesiredLength = Convert.ToInt32(Console.ReadLine());
+        int DesiredLength = Convert.ToInt32(Console.ReadLine()); //Input
 
         int temp;
         Random random = new Random();
@@ -12,30 +12,30 @@
         char[] SpecialChars = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', ';', ':', '\'', '"', ',', '<', '.', '>', '/', '?', '`', '~' };
         char[] Numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-        for (int i = 1; i <= DesiredLength; i++)
+        for (int i = 1; i <= DesiredLength; i++) // Generate characters based on desired legnth
         {
-            switch (random.Next(0, 3))
+            switch (random.Next(0, 3))  // choose wether to generate a number, special character or letter
             {
-                case 0:
-                    switch (random.Next(0, 2))
+                case 0: // Letter
+                    switch (random.Next(0, 2)) // Choose between lowercase and uppercase
                     {
-                        case 0:
+                        case 0: // Upper (default)
                             Password = Password + Letters[random.Next(0, Letters.Length - 1)];
                             break;
-                        case 1:
+                        case 1: // Lower
                             string RandomLetter = Convert.ToString(Letters[random.Next(0, Letters.Length - 1)]).ToLower();
                             Password = Password + RandomLetter;
                             break;
                     }
                     break;
-                case 1:
+                case 1: // Special character
                     Password = Password + Convert.ToString(SpecialChars[random.Next(0, SpecialChars.Length - 1)]);
                     break;
-                case 2:
+                case 2: // number
                     Password = Password + Convert.ToString(Numbers[random.Next(0, Numbers.Length - 1)]);
                     break;
             }
         }
-        Console.WriteLine(Password);
+        Console.WriteLine(Password); // output
     }
 }
